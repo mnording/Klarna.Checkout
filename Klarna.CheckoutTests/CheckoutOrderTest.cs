1427cc4a97f918cd7b67c044a64fd16d4bec5044
Mattias Nording <mattiasnording@gmail.com>
2017-09-22 14:01:55 +0200

    Solved bug in tax calculation

---
+++ Klarna.CheckoutTests/CheckoutOrderTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Klarna.Checkout;
+using Klarna.Checkout.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Klarna.CheckoutTests
+{
+    [TestClass]
+    public class CheckoutOrderTest
+    {
+        [TestMethod]
+        public void MustCalculateOrderTaxAmountCorrect()
+        {
+            var orderlines = new List<CheckoutOrderLine>
+            {
+                new CheckoutOrderLine("test", 2, 1000, 1000),
+                new CheckoutOrderLine("test", 2, 1000, 1000),
+                new CheckoutOrderLine("test", 2, 1000, 1000)
+            };
+            CheckoutOrder order = new CheckoutOrder(orderlines,null);
+            Assert.AreEqual(546,order.OrderTaxAmount);
+        }
+        [TestMethod]
+        public void MustCalculateOrderAmountCorrect()
+        {
+            var orderlines = new List<CheckoutOrderLine>
+            {
+                new CheckoutOrderLine("test", 2, 1000, 1000),
+                new CheckoutOrderLine("test", 2, 1000, 1000),
+                new CheckoutOrderLine("test", 2, 1000, 1000)
+            };
+            CheckoutOrder order = new CheckoutOrder(orderlines, null);
+            Assert.AreEqual(6000, order.OrderAmount);
+        }
+    }
+}
